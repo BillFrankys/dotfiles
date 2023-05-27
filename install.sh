ead62cfe68841a1d3c839026210ac326a017672a
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-27 23:25:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
+set -x
 curl https://sh.rustup.rs | sh -s -- -y 
 source ~/.cargo/env
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 cargo binstall -y --force cargo-update nu
 
 rm -rf $HOME/.dotfiles
-git clone --bare https://github.com/BillFrankys/dotfiles  $HOME/.dotfiles
-git config --global alias.dtf '!git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-git --git-dir=$HOME/.dotfiles/ config --local status.showUntrackedFiles no
-git dtf checkout -f
+rm -rf $HOME/.local/share/.dotfiles
+mkdir -p $HOME/.local/share/
+git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/.dotfiles" --depth 1
+git --git-dir="$HOME/.local/share/.dotfiles" config --local status.showUntrackedFiles no
+git --git-dir="$HOME/.local/share/.dotfiles" --work-tree="$HOME" checkout -f
+
 
 # rm -rf HOME/.dotfiles
 # git clone --bare https://github.com/BillFrankys/dotfiles  $HOME/.dotfiles
