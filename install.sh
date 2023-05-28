ec49a92558158e966a62dfa2cd0a7a713b0cb728
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 06:01:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 export CARGO_HOME="$HOME/.local/share/cargo"
 
 rm -rf $HOME/.local
@@ -13,11 +12,19 @@ source $CARGO_HOME/env
 
 curl -o- -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
+echo "## Clone dotfiles:"
 git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/repos/dotfiles" --depth 1
-git config --global alias.dotfiles '!git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam'
-git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam config --local status.showUntrackedFiles no
+
+echo "## Checkout dotfiles:"
 git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam checkout -f
 
-cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
+echo "## Config git dotfiles alias:"
+git config --global alias.dotfiles '!git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam'
+
+echo "## Config git untracked files:"
+git dotfiles config --local status.showUntrackedFiles no
+
+
+#cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
 
  
