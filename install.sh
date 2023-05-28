87cd5365e067f88b305310e2c6c3ea61c730dadf
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 06:10:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,19 @@ source $CARGO_HOME/env
 
 curl -o- -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-echo "## Clone dotfiles:"
+echo "### Clone dotfiles:"
 git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/repos/dotfiles" --depth 1
 
-echo "## Checkout dotfiles:"
+echo "### Checkout dotfiles:"
 git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam checkout -f
 
-echo "## Config git dotfiles alias:"
+echo "### Config git dotfiles alias:"
 git config --global alias.dotfiles '!git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam'
 
-echo "## Config git untracked files:"
+echo "### Config git untracked files:"
 git dotfiles config --local status.showUntrackedFiles no
 
-
-#cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
+echo "### Config git untracked files:"
+CARGO_HOME="$HOME/.local/share/cargo"  cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
 
  
