6b2d92f7e91266255b9369adc34bf4e42b4cfa5d
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 04:44:54 +0300

    111

--- install.sh
+++ install.sh
@@ -13,20 +13,11 @@ curl https://sh.rustup.rs | CARGO_HOME="$HOME/.local/share/cargo" sh -s -- -y
 
 source $CARGO_HOME/env
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-cargo binstall -y --force cargo-update nu
-
-
-
 git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/.dotfiles" --depth 1
 git config --global alias.dtf '!git --git-dir=/home/dryam/.local/share/.dotfiles --work-tree=/home/dryam'
 git --git-dir=/home/dryam/.local/share/.dotfiles --work-tree=/home/dryam config --local status.showUntrackedFiles no
 git --git-dir=/home/dryam/.local/share/.dotfiles --work-tree=/home/dryam checkout -f
 
+cargo binstall -y --force cargo-update nu
 
-# rm -rf HOME/.dotfiles
-# git clone --bare https://github.com/BillFrankys/dotfiles  $HOME/.dotfiles
-# git --git-dir=$HOME/.dotfiles/ config --local status.showUntrackedFiles no
-
-# alacritty alacritty-opacity asdf atuin carapace cargo colortty fd felix fzf helix hyperfine nerdfonts nu pastel pueue ripgrep starship trashy vivid wl-clipboard zoxide
  
