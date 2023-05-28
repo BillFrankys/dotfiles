56af0e5ac7575d271acd764fa75185959b95dc16
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 05:33:02 +0300

    111

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ cd ~/.local && curl https://github.com/Kitware/CMake/releases/download/v3.26.4/c
 
 curl https://sh.rustup.rs | CARGO_HOME="$HOME/.local/share/cargo" sh -s -- -y 
 
-source $CARGO_HOME/env
+. $CARGO_HOME/env
 
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+curl -o- -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/repos/dotfiles" --depth 1
 git config --global alias.dtf '!git --git-dir=/home/dryam/.local/share/repos/dotfiles --work-tree=/home/dryam'
 git --git-dir=/home/dryam/.local/share/repose/dotfiles --work-tree=/home/dryam config --local status.showUntrackedFiles no
 git --git-dir=/home/dryam/.local/share/repose/dotfiles --work-tree=/home/dryam checkout -f
 
-cargo binstall -y --force cargo-update nu
+cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
 
  
