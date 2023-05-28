63959565b822ab6da13c362c8abd113315bdd99e
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 08:27:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ mkdir -p $HOME/.local
 echo "### Installing cmake:"
 cd ~/.local &&  curl   -L --proto '=https' --tlsv1.2 -sSf https://github.com/Kitware/CMake/releases/download/v3.26.4/cmake-3.26.4-linux-x86_64.sh --output cmake-3.26.4-linux-x86_64.sh  && bash ./cmake-3.26.4-linux-x86_64.sh --skip-license
 
+echo "### Installing rustup:"
 curl https://sh.rustup.rs | CARGO_HOME="$HOME/.local/share/cargo" sh -s -- -y 
 
 source $CARGO_HOME/env
 
+echo "### Installing cargo-binstall:"
 curl -o- -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 echo "### Clone dotfiles:"
@@ -25,7 +27,11 @@ git config --global alias.dotfiles '!git --git-dir=/home/dryam/.local/share/repo
 echo "### Config git untracked files:"
 git dotfiles config --local status.showUntrackedFiles no
 
-echo "### Config git untracked files:"
-CARGO_HOME="$HOME/.local/share/cargo"  cargo binstall -y --force cargo-update nu alacritty alacritty-opacity asdf atuin colortty fd-find felix  hyperfine pastel pueue ripgrep starship trashy vivid  zoxide
+echo "### Binstall apps:"
+CARGO_HOME="$HOME/.local/share/cargo"  cargo binstall -y --force cargo-update nu asdf atuin fd-find pueue ripgrep starship trashy vivid  zoxide
 
- 
+echo "### Install helix"
+curl -o- -L --proto '=https' --tlsv1.2 -sSf https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-linux.tar.xz  | tar xfJ - -C /tmp
+cp -r /tmp/helix-23.05-x86_64-linux/runtime ~/.config/helix/
+cp -r /tmp/helix-23.05-x86_64-linux/hx ~/.local/bin/
+rm -rf /tmp/helix-23.05-x86_64-linux/hx
