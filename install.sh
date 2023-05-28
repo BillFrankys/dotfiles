ecce4de125ae4348f185d5c438728a5dc02b517e
BillFrankys <133805384+BillFrankys@users.noreply.github.com>
2023-05-28 03:13:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,20 @@ export CARGO_HOME="$HOME/.local/share/cargo"
 curl https://sh.rustup.rs | CARGO_HOME="$HOME/.local/share/cargo" sh -s -- -y 
 source $CARGO_HOME/env
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+curl https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_amd64.tar.gz
+
+mkdir -p $HOME/.local/bin
+tar xvfz gh_2.29.0_linux_amd64.tar.gz  */bin/gh
+cp gh_2.29.0_linux_amd64/bin/gh ~/.local/bin
+rm -rf gh_2.29.0_linux_amd64
 cargo binstall -y --force cargo-update nu
 
+rm -rf $HOME/.dotfiles
 rm -rf $HOME/.local/share/.dotfiles
 mkdir -p $HOME/.local/share/
 mkdir -p $HOME/.local/bin
 
-
+gh auth login  --web
 git clone --bare https://github.com/BillFrankys/dotfiles  "$HOME/.local/share/.dotfiles" --depth 1
 git config --global alias.dtf '!git --git-dir=/home/dryam/.local/share/.dotfiles --work-tree=/home/dryam'
 git --git-dir=/home/dryam/.local/share/.dotfiles --work-tree=/home/dryam config --local status.showUntrackedFiles no
